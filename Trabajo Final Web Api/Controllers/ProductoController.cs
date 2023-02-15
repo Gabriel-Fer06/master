d3ed71f3014c5de6c01041f162adaa63dc5d11f9
Gabriel-Fer06 <122941292+Gabriel-Fer06@users.noreply.github.com>
2023-02-15 13:03:28 -0300

    Add files via upload
    Trabajo final Web Api ded Fernández Gabriel

---
+++ Trabajo Final Web Api/Controllers/ProductoController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestionarProductosWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        [HttpPost("/api/Producto")]
+        public void InsertarProducto(Producto producto)
+        {
+            ManejadorProducto.CrearProducto(producto);
+
+        }
+
+        [HttpPut("/api/Producto")]
+        public void ModificarProducto(Producto producto)
+        {
+            ManejadorProducto.ModificarProducto(producto);
+        }
+
+        [HttpDelete("/api/Producto/{id}")]
+        public void EliminarProducto(long id)
+        {
+            ManejadorProducto.EliminarProducto(id);
+        }
+
+        [HttpGet("/api/Producto/{idUsuario}")]
+        public List<Producto> TraerProductos(long idUsuario)
+        {
+            return ManejadorProducto.ObtenerProductos(idUsuario);
+        }
+    }
+}
